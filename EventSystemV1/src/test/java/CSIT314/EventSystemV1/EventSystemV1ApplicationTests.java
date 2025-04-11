2926cadea8054ad7d6cd5bb506b097c93e87ffc4
jz763 <jz763@uowmail.edu.au>
2025-04-11 14:08:12 +1000

    Project creation

---
+++ EventSystemV1/src/test/java/CSIT314/EventSystemV1/EventSystemV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package CSIT314.EventSystemV1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSystemV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
