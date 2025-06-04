8a0045bf31056b8f48fd8c6e3680395db1eb23fc
Awkward-turtleness <pm426@uowmail.edu.au>
2025-06-04 20:48:22 +1000

    fixed merge conflicts app tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,4 @@
 import { HashRouter as Router, Routes, Route } from "react-router";
-<<<<<<< HEAD
-import { Login } from "./Pages/Login";
-import { Signup } from "./Pages/Signup";
-import { Navbar } from "./assets/Components/Navbar";
-import TestPage from "./Pages/TestPage";
-
-function App() {
-  return (
-    <>
-      <div>
-        {/* Temporary */}
-        <Navbar />
-
-        <Router>
-          <Routes>
-            <Route>
-              <Route path="/" element={<Login />} />
-              <Route path="/Registration" element={<Signup />} />
-              <Route path= "/testpage" element= {<TestPage/>}></Route>
-            </Route>
-          </Routes>
-        </Router>
-      </div>
-    </>
-=======
 import { Login } from "./Pages/User/Login";
 import { Signup } from "./Pages/User/Signup";
 import { HomePage } from "./Pages/User/HomePage";
@@ -43,7 +18,7 @@ function App() {
         <Route path="/PaymentPage" element={<PaymentPage />} />
       </Routes>
     </Router>
->>>>>>> refs/remotes/master/master
+
   );
 }
 
