629185167dcacec18964de360fdf8c197a882ca2
Probster115 <ampeminara@gmail.com>
2025-06-04 17:25:32 +1000

    Added HTML, CSS and a bit of typescript for Event Details and Search Results

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,21 @@
 import { HashRouter as Router, Routes, Route } from "react-router";
-import { Login } from "./Pages/Login";
-import { Signup } from "./Pages/Signup";
-import { Navbar } from "./assets/Components/Navbar";
-import TestPage from "./Pages/TestPage";
+import { Login } from "./Pages/User/Login";
+import { Signup } from "./Pages/User/Signup";
+import { HomePage } from "./Pages/User/HomePage";
+import { SearchResults } from "./Pages/User/SearchResults";
+import { Event } from "./Pages/User/Event";
 
 function App() {
   return (
-    <>
-      <div>
-        {/* Temporary */}
-        <Navbar />
-
-        <Router>
-          <Routes>
-            <Route>
-              <Route path="/" element={<Login />} />
-              <Route path="/Registration" element={<Signup />} />
-              <Route path= "/testpage" element= {<TestPage/>}></Route>
-            </Route>
-          </Routes>
-        </Router>
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/Registration" element={<Signup />} />
+        <Route path="/HomePage" element={<HomePage />} />
+        <Route path="/SearchResults" element={<SearchResults />} />
+        <Route path="/Event" element={<Event />} />
+      </Routes>
+    </Router>
   );
 }
 
