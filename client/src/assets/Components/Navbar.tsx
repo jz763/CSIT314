097cfe9000ecde2e60b4ec34631839714c6bf379
Awkward-turtleness <pm426@uowmail.edu.au>
2025-06-04 20:50:53 +1000

    fixed navbar conflicts

--- client/src/assets/Components/Navbar.tsx
+++ client/src/assets/Components/Navbar.tsx
@@ -1,45 +1,12 @@
 import logo from "../Images/logo.png";
-<<<<<<< HEAD
 
-=======
->>>>>>> refs/remotes/master/master
+
 import bookmark_icon from "../Images/bookmark-empty.png";
 import profile_pic from "../Images/Profile Pic.png";
 import loc_icon from "../Images/location-sharp.png";
 import search_icon from "../Images/search.png";
-<<<<<<< HEAD
+
 import styles from "./Navbar.module.css";
-
-export const Navbar = () => {
-  return (
-    <header className={styles["header"]}>
-      <a href="/">
-        <div className={styles["logo-wrapper"]}>
-          <img className={styles["logo"]} src={logo} alt="" />
-        </div>
-      </a>
-      <div className={styles["left-group"]}>
-        <div className={styles["search"]}>
-          <div className={styles["search-events"]}>
-            <input
-              className={styles["input"]}
-              type="text"
-              placeholder="Search Events"
-            />
-            <img src={search_icon} />
-          </div>
-          <div className={styles["location"]}>
-            <input
-              className={styles["input"]}
-              type="text"
-              placeholder="Location"
-            />
-            <img src={loc_icon} />
-          </div>
-        </div>
-        <a className={styles["aboutUs"]} href="/">
-=======
-
 export const Navbar = () => {
   return (
     <header
@@ -124,25 +91,25 @@ export const Navbar = () => {
           href="/"
           className="text-white text-[24px] font-normal hover:underline"
         >
->>>>>>> refs/remotes/master/master
+
           About Us
         </a>
       </div>
 
-<<<<<<< HEAD
+
       <div className={styles["user_bookmark"]}>
         <a href="/">
           <img src={bookmark_icon} />
         </a>
         <img src={profile_pic} />
-=======
+      </div>
       {/* User & Bookmark on the right */}
       <div className="flex items-center justify-center">
         <a href="/">
           <img src={bookmark_icon} alt="Bookmark icon" />
         </a>
         <img src={profile_pic} alt="Profile picture" className="ml-[35px]" />
->>>>>>> refs/remotes/master/master
+
       </div>
     </header>
   );
