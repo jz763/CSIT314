51d2c5c07e88cab3cdcda7263ce71c694cf433c6
Probster115 <ampeminara@gmail.com>
2025-06-06 19:24:59 +1000

    Added Refund Button

--- client/src/Pages/User/TicketPage.tsx
+++ client/src/Pages/User/TicketPage.tsx
@@ -6,9 +6,9 @@ import { Ticket } from "../../assets/Components/Ticket";
 
 export const TicketPage = () => {
   return (
-    <div>
-      <div className="flex justify-center items-center text-[50px] text-white font-bold mt-[150px]">
-        Your Ticket
+    <div className="bg-[#1d1c1c] min-h-screen">
+      <div className="flex justify-center items-center text-[50px] text-white font-bold mt-[110px]">
+        <div className="mt-10">Your Tickets</div>
       </div>
       <div className="flex justify-center items-center pt-10">
         <Ticket />
